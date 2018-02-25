1b10001b268368f5e5de3f7fbffbc91072498787
Aybüke Erata <32402465+aybukeerata@users.noreply.github.com>
2018-02-25 11:50:56 +0300

    Update post.rb

--- _plugins/post.rb
+++ _plugins/post.rb
@@ -1,6 +1,8 @@
 module Jekyll
 
-  class Post
+  class Document
+
+    #Updated for Jekyll 3.0.0
 
     # override post method in order to return categories names as slug
     # instead of strings
@@ -10,21 +12,28 @@ module Jekyll
     # instead of url encoded form : /category%20with%20space
     #
     # @see utils.slugify
+
     def url_placeholders
       {
-          :year        => date.strftime("%Y"),
-          :month       => date.strftime("%m"),
-          :day         => date.strftime("%d"),
-          :title       => slug,
-          :i_day       => date.strftime("%-d"),
-          :i_month     => date.strftime("%-m"),
-          :tags        => (tags || []).map { |c| Utils.slugify(c) }.join('/'),
-          :categories  => (categories || []).map { |c| Utils.slugify(c) }.join('/'),
-          :short_month => date.strftime("%b"),
-          :short_year  => date.strftime("%y"),
-          :y_day       => date.strftime("%j"),
-          :output_ext  => output_ext
+        collection:  collection.label,
+        path:        cleaned_relative_path,
+        output_ext:  output_ext,
+        name:        Utils.slugify(basename_without_ext),
+        title:       Utils.slugify(data['slug']) || Utils.slugify(basename_without_ext),
+        year:        date.strftime("%Y"),
+        month:       date.strftime("%m"),
+        day:         date.strftime("%d"),
+        hour:        date.strftime("%H"),
+        minute:      date.strftime("%M"),
+        second:      date.strftime("%S"),
+        i_day:       date.strftime("%-d"),
+        i_month:     date.strftime("%-m"),
+        categories:  (data['categories'] || []).map { |c| Utils.slugify(c) }.uniq.join('/'),
+        short_month: date.strftime("%b"),
+        short_year:  date.strftime("%y"),
+        y_day:       date.strftime("%j"),
       }
+
     end
 
   end
